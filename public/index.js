bc81ba107f226eefa600a39dd17c93ecf50ce312
simone <sorzesimone1@gmail.com>
2020-12-14 21:57:43 +0100

    added emojy and updated code with animate.css

--- public/index.js
+++
@@ -1,9 +0,0 @@
-const text_effect = () => {
-  const animation = document.getElementById("titleAnimation").innerHTML;
-  const first_letter = animation.charAt(0);
-  const second_letter = animation.charAt(1);
-  const third_letter = animation.charAt(3);
-  console.log(first_letter, second_letter, third_letter);
-};
-
-text_effect();
