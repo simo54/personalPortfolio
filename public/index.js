220887973f26eec0c7e9877740d6743a94da207a
simone <sorzesimone1@gmail.com>
2020-12-17 14:33:04 +0100

    update copyclipboard, inserted new section

--- public/index.js
+++ public/index.js
@@ -1,23 +1,13 @@
+// Copying the list of stack on the clipboard
 const copy2clipboard = document.getElementById("copy2clipboard");
-let text2clipboard = document.getElementById("text2clipboard");
+const text2clipboard = document.getElementById("text2clipboard");
+const tooltiptext = document.getElementById("tooltiptext");
 
 copy2clipboard.addEventListener("click", () => {
   let text2copy = text2clipboard.value.trim();
-  if (text2copy) {
-    navigator.clipboard
-      .writeText(text2copy)
-      .then(() => {
-        text2copy.value = "";
-        if (copy2clipboard.innerText !== "Copied!") {
-          const originalText = copy2clipboard.innerText;
-          //   copy2clipboard.innerText = "Copied!";
-          setTimeout(() => {
-            copy2clipboard.innerText = originalText;
-          }, 1500);
-        }
-      })
-      .catch((err) => {
-        console.log("Something went wrong", err);
-      });
-  }
+  tooltiptext.style.display = "block";
+  navigator.clipboard.writeText(text2copy);
+  setTimeout(() => {
+    tooltiptext.style.display = "none";
+  }, 3000);
 });
